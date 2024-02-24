ef57414c7f551dfec9ea76324999b52159b31043
brandon <brandonli777xd@gmail.com>
2024-02-24 13:50:18 -0500

    variables

--- variables.js
+++ variables.js
@@ -1,5 +1,33 @@
-var edad = 27;
-console.log(edad); 
+/**tipos de datos que podemos guardar en variables **/
+// string - cadena de texto
+// Number - numero 
+// las variable que todo el tiempo voy a utlizat va a ser const y let 
+
+
+
+var edad = 27; 
+let nombre = 'carlos';
+const correo = 'correo@correo.com';
+
+console.log(nombre);
+
+let telefono, pais , id;
+telefono = 12345678;
+// console.log(telefono)
+
+
+const resultado = 4 + 4;
+
+
+const nombre1 = 'carlos';
+const nombre2 = 'arturo';
+const nombrecompleto = nombre1 + nombre2;
+
+let mivariable = 'texto' ;
+mivariable = 7;
+
+
+
 
 
 
