ce1103b4e67e9a6d87b6b60335bb5f828f568d8e
brandon <brandonli777xd@gmail.com>
2024-02-24 17:54:09 -0500

    operadores

---
+++ operadores.js
@@ -0,0 +1,51 @@
+/* operadores aritmeticos
+ = operador de asignacion, se usa para asignar valores a una variable
+ +  suma
+ -  resta 
+ *  multiplicacion
+ /  division 
+ %  modulo
+ ++  aumento
+ --  disminucion
+ */
+
+
+//  const resultado = 10 + 10;
+//  const resultado2 = 10 - 10;
+//  const resultado3 = 10 % 3;
+
+
+// let numero = 1;
+// numero--;
+
+
+
+//  console.log(numero);
+
+ /* operadores de asignacion
+
+ += suma de numero al valor de una variable 
+ -= resta un numero al valor de una variable 
+ *= restaun numero al valor de una variable 
+ /= resta un numero al valor de una variable.
+ %= obtiene el sobrante de una division  y  lo asigna a la variante.
+ */
+
+
+ let numero = 10; 
+//  numero = numero + 5;
+numero -= 5;
+
+// operadores de comparacion
+// const resultado  = 20 <= 20; // true
+// const resultado  = 10 == 10; // true
+// const resultado  = 10 === '10';
+
+const resultado = 7 > 10 ? 'el primer valor es mayor que el segundo' : 'el segundo valor es mayor que el primero ';
+console.log(resultado);
+
+
+
+
+ 
+
