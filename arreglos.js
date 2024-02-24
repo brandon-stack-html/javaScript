afa2c3dc704919984f94adf86955ac050e648360
brandon <brandonli777xd@gmail.com>
2024-02-24 18:30:34 -0500

    arreglos

---
+++ arreglos.js
@@ -0,0 +1,16 @@
+const arreglo = ['texto', 456.10, false, {propiedades: 'valor'}];
+console.log (arreglo);
+
+const amigos = ['alejandro', 'manuel', 'cesar'];
+console.log(amigos[0]);
+
+const colores = [];
+colores[0] = 'rojo';
+colores[1] = 'verde';
+colores[3] = 'blanco';
+colores[3] = 'amarillo'; 
+
+console.log('el arreglo colores tiene: ' + colores.length + ' colores');
+
+colores.push('azul');
+console.log(colores);
