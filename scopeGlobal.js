2f70adfb889fb833791682c24b2c798c85e8b090
brandon <brandonli777xd@gmail.com>
2024-02-25 15:26:14 -0500

    scopeglobal

---
+++ scopeGlobal.js
@@ -0,0 +1,17 @@
+const edad = 19;
+if (edad >= 18) {
+    const accesoPermitido = true;
+    if(true) {
+        console.log(accesoPermitido);
+    }
+    
+    const miFuncion = () => {
+        console.log(accesoPermitido);
+    };
+    miFuncion();
+}
+
+const accesoPermitido = 'SI';
+console.log(accesoPermitido);
+
+
