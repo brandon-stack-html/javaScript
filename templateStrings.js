d03d833c7d2b078a9862854036fd871ffa5e17aa
brandon <brandonli777xd@gmail.com>
2024-02-25 09:19:50 -0500

    template strings 2

---
+++ templateStrings.js
@@ -0,0 +1,8 @@
+const nombre = 'carlos arturo';
+const edad = 27;
+const pais = 'mexico';
+
+console.log('la persona es ' + nombre + ' tiene ' + edad + ' y es de ' + pais);
+
+console.log(`la persona es ${nombre} tiene ${edad} y vive en ${pais}`)  
+
