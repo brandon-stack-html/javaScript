07efc67ab570e6e3e5484414b97dca1304b2103e
brandon <brandonli777xd@gmail.com>
2024-02-25 11:53:42 -0500

    funciones

---
+++ funciones.js
@@ -0,0 +1,14 @@
+//  forma 1
+// function saludo() {
+//     console.log ('hola');
+// }
+
+// const variable = saludo;
+
+// forma 2 - asignado una fucion a una variable
+// const saludo = function () {
+//     console.log('hola desde variable');
+// };
+
+// forma 3 - funcion de tipo flecha. 
+const saludo = () =>  {console.log('hola');};saludo();
